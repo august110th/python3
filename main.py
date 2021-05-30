45d5acd91168f49ccc4ea69c9d5b5f1aa2240d2d
august110th <august110th@ya.ru>
2021-05-30 22:57:03 +0400

    done

--- main.py
+++ main.py
@@ -3,31 +3,35 @@ import json
 import requests
 
 
-# with open('countries.json') as f:
-#     templates = json.load(f)
-#     print(templates[1]['name']['common'])
+with open('countries.json') as f:
+    templates = json.load(f)
+    data = json.loads(templates.text)
+    data = json.dumps(data)
+
+    print(data)
+    # names = re.findall(r'(?<="common" :\s").+(?=",[\s\S]+"official")', templates)
+    # print(names)
 
 
 
-json_one = "https://raw.githubusercontent.com/mledoze/countries/master/countries.json"
-
-
-class Json_one_iterate():
-
-    def __iter__(self):
-        self.counter = 1
-        with open('countries.json') as f:
-            templates = json.load(f)
-            self.x = (templates[self.counter]['name']['common'])
-            print(self.x)
-        return self
-
-    def __next__(self):
-        self.counter += 1
-        link = next(self.x)
-        return link
-
-
-pages = Json_one_iterate()
-for names in pages:
-    print(names)
+# json_one = "https://raw.githubusercontent.com/mledoze/countries/master/countries.json"
+#
+#
+# class Json_one_iterate():
+#
+#     def __iter__(self):
+#         self.counter = 1
+#         with open('countries.json') as f:
+#             templates = json.load(f)
+#             self.x = (templates[self.counter]['name']['common'])
+#             # print(self.x)
+#         return self
+#
+#     def __next__(self):
+#         return self
+#
+#
+#
+# pages = Json_one_iterate()
+# for names in pages:
+#     print(names)
