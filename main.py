941a504859eb869c95072d6280b7b79eeb4996c6
august110th <august110th@ya.ru>
2021-05-30 19:45:53 +0400

    done

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import re
-
+import json
 import requests
-json_one = "https://raw.githubusercontent.com/mledoze/countries/master/countries.json"
+import pprint
 
-class Json_one_iterate():
-    def __iter__(self):
-        response = requests.get(json_one)
-        main_page = response.text
-        self.links = iter(set(re.findall(r'"name": "common":', main_page)))
-        return self
-    def __next__(self):
-        link = next(self.links)
-        return requests.get(link).text
+with open('countries.json') as f:
+    templates = json.load(f)
+    x = re.findall("name", templates)
+    print(x)
 
-pages = tuple(Json_one_iterate())
-print(pages)
+
+
+# json_one = "https://raw.githubusercontent.com/mledoze/countries/master/countries.json"
+
+
+# class Json_one_iterate():
+#     def __iter__(self):
+#         with open('countries.json') as f:
+#             templates = json.load(f)
+#             x = iter(set(re.findall(r'', templates)))
+#             # print(x)
+#         return self
+#         # response = requests.get(json_one)
+#         # main_page = response.text
+#
+# #         print(main_page)
+# #         self.links = iter(set(re.findall(r'"common": "(.*?)","official"', main_page)))
+# #         return
+# #
+# #     def __next__(self):
+# #         link = next(self.links)
+# #         return requests.get(link).text
+# # #
+# #
+# pages = tuple(Json_one_iterate())
+# print(pages)
+# # pages = Json_one_iterate
+# # for item in pages:
+# #     print(item)
