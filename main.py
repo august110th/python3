6ea459a77b64701d79f07c0f12a6b25bcfb0e5cd
august110th <august110th@ya.ru>
2021-06-01 20:54:09 +0400

    done

--- main.py
+++ main.py
@@ -3,39 +3,25 @@ import json
 import requests
 
 #
-with open('countries.json') as f:
-    templates = json.load(f)
-    data = json.dumps(templates)
-    # print(data)
+# with open('countries.json') as f:
+#     templates = json.load(f)
+#     data = json.dumps(templates)
+#     # print(data)
+#
+#     names = re.findall('("name": {"common":)''(.*?)''("official":)' ,data)
+#     print(names)
 
-    names = re.findall('("name": {"common":)''(.*?)''("official":)' ,data)
+class Json_one_iterate():
+    def __iter__(self):
+        with open('countries.json') as f:
+            data = json.load(f)
+            self.x = re.findall('("name": {"common":)''(.*?)''("official":)', data)
+            # print(self.x)
+        return self
+    def __next__(self):
+        t = next(self.x)
+        return t
+
+pages = Json_one_iterate()
+for names in pages:
     print(names)
-# #
-# #
-# #
-# # # json_one = "https://raw.githubusercontent.com/mledoze/countries/master/countries.json"
-# # #
-# # #
-# # class Json_one_iterate():
-# #     def __init__(self):
-# #         self.counter = 1
-# #
-# #     def __iter__(self):
-# #         with open('countries.json') as f:
-# #             templates = json.load(f)
-# #             self.x = (templates[self.counter]['name']['common'])
-# #             # print(self.x)
-# #         return self
-# #     def __next__(self):
-# #         self.counter += 1
-# #         t = next(self.x)
-# #         return t
-# # # #
-# # # #
-# # # #
-# # pages = Json_one_iterate()
-# # for names in pages:
-# #     print(names)
-# # # with open('countries.json') as f:
-# # #     templates = json.load(f)
-# # #     print(templates[2]["name"]["common"])
