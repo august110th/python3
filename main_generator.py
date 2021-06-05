fe387f901ebe8a6a96fbd93ff2d33ef8fc8da461
august110th <august110th@ya.ru>
2021-06-05 15:45:21 +0400

    done

--- main_generator.py
+++ main_generator.py
@@ -1,24 +1,11 @@
 import json
 import hashlib
 
-# def generator():
-#     with open('countries.json') as f:
-#         names = json.load(f)
-#     for line in names:
-#         yield line
-#         hashlib.md5(line).hexdigest()
-
-
-# for x in generator():
-#     print(type(x))
-# # print(d)
-
-# hash_object = hashlib.md5(b'Hello World')
-# print(hash_object.hexdigest())
-
-with open('countries.json') as f:
-    names = json.load(f)
-    # print(type(names))
+def generator():
+    with open('countries.json') as f:
+        names = json.load(f)
     for line in names:
-        h = hashlib.md5(str(line).encode('utf-8'))
-        print(h)
+        yield hashlib.md5(str(line).encode('utf-8')).hexdigest()
+
+for hash in generator():
+    print(hash)
